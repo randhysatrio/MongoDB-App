72cd0c9d0b02ece6ae5040927bd58a9823a9fc22
randhysatrio <randhysatrio@gmail.com>
2022-03-02 13:04:58 +0700

    Update PORT and package.json configuration

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const cors = require('cors');
 const mongoose = require('mongoose');
 
 const app = express();
-const PORT = 8000;
+const PORT = process.env.PORT || 8000;
 
 app.use(cors());
 app.use(express.json());
